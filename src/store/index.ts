d98737dd3d370d3bf6f5a5b63846428e6bfb3653
likui628 <likui.it@gmail.com>
2021-10-04 09:40:07 +0800

    add vuex suport

---
+++ src/store/index.ts
@@ -0,0 +1,22 @@
+import { InjectionKey } from 'vue'
+import { createStore, useStore as baseUseStore, Store } from 'vuex'
+
+export interface State {
+  count: number
+}
+export const key: InjectionKey<Store<State>> = Symbol()
+
+export const store = createStore<State>({
+  state: {
+    count: 0,
+  },
+  mutations: {
+    increment(state: any) {
+      state.count += 1
+    },
+  },
+})
+
+export function useStore() {
+  return baseUseStore(key)
+}
